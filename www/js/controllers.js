b0299b8857e911bbed2b9f2de37e762c63c882b9
Macmini <Macmini@Macminis-MacBook-Air.local>
2017-03-25 05:11:55 +0530

    Added my project

---
+++ www/js/controllers.js
@@ -0,0 +1,337 @@
+angular.module('app.controllers', [])
+
+.controller('HomePageCtrl', ['$scope', '$stateParams','$state','$ionicLoading','getPost','Cat_list',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope,$stateParams,$state,$ionicLoading,getPost,Cat_list) {
+	$ionicLoading.show({
+		content: 'Loading',
+		animation: 'fade-in',
+		showBackdrop: true,
+		maxWidth: 200,
+		showDelay: 0
+	});
+	// Cat_list.cat_list().then(function(data){
+	// 	var ret=JSON.parse(data);
+	// 	$scope.cat=ret.result;
+	// })
+
+	// getPost.get_post().then().then(function(data){
+	// 	$ionicLoading.hide();
+	// 	var ret=JSON.parse(data);
+	// 	$scope.listdata=ret.result;
+	// })
+	// $scope.fullDesc=function(index,id){
+	// 	$state.go('tabsController.detail',{val_index:id});
+	// }
+	
+
+	// $scope.category="-Select-";
+
+	// $scope.show_category=function(category){
+	// 	$ionicLoading.show({
+	// 		content: 'Loading',
+	// 		animation: 'fade-in',
+	// 		showBackdrop: true,
+	// 		maxWidth: 200,
+	// 		showDelay: 0
+	// 	});
+	// 	if(category==$scope.category){
+	// 		getPost.get_post().then().then(function(data){
+	// 			$ionicLoading.hide();
+	// 			var ret=JSON.parse(data);
+	// 			$scope.listdata=ret.result;
+	// 		})
+	// 		$scope.fullDesc=function(index,id){
+	// 			$state.go('tabsController.detail',{val_index:id});
+	// 		}
+	// 	}
+	// 	else{
+	// 		getPost.get_post(category).then().then(function(data){
+	// 			$ionicLoading.hide();
+	// 			var ret1=JSON.parse(data);
+	// 			$scope.listdata=ret1.result;
+	// 		})
+	// 		$scope.fullDesc=function(index,id){
+	// 			$state.go('tabsController.detail',{val_index:id});
+	// 		}
+
+	// 	}
+	// }
+
+}])
+
+.controller('detailCtrl', ['$scope', '$stateParams','$state','$ionicLoading','getPost_Byid','$cordovaSocialSharing', function($scope,$stateParams,$state,$ionicLoading,getPost_Byid,$cordovaSocialSharing){
+	//alert($state.params.val_index);
+	$ionicLoading.show({
+		content: 'Loading',
+		animation: 'fade-in',
+		showBackdrop: true,
+		maxWidth: 200,
+		showDelay: 0
+	});
+	getPost_Byid.get_postid($state.params.val_index).then().then(function(data){
+		var ret=JSON.parse(data);
+		$ionicLoading.hide();
+		$scope.listdata=ret.result[0];
+		$scope.socialShare=function(){
+			$cordovaSocialSharing
+    .share(ret.result[0].discription, 'subject', null) // Share via native share sheet
+    .then(function(result) {
+      // Success!
+  }, function(err) {
+      // An error occured. Show a message to the user
+  });
+}
+})
+
+}])
+
+.controller('aboutPageCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams) {
+	
+
+}])
+
+.controller('servicesPageCtrl', ['$scope','$stateParams','$state','$ionicLoading','Email_send','Sub_category','Cat_list','subList_byCat','list_service', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams,$state,$ionicLoading,Email_send,Sub_category,Cat_list,subList_byCat,list_service) {
+	$ionicLoading.show({
+		content: 'Loading',
+		animation: 'fade-in',
+		showBackdrop: true,
+		maxWidth: 200,
+		showDelay: 0
+	});
+	$scope.subcategory="-Select-";
+	$scope.category="-Select-";
+	list_service.Service_list().then(function(data){
+		$ionicLoading.hide();
+		var ret_data=JSON.parse(data);
+		$scope.cat=ret_data.result;
+	});
+
+	$scope.show_category=function(id){
+		$ionicLoading.show({
+			content: 'Loading',
+			animation: 'fade-in',
+			showBackdrop: true,
+			maxWidth: 200,
+			showDelay: 0
+		});
+		subList_byCat.List_byCat(id).then(function(data){
+			$ionicLoading.hide();
+			//console.log(data.trim());
+			var ret_data=JSON.parse(data);
+
+			
+			if(ret_data.result==""){
+				$scope.sub=true;
+			}
+			else{
+				$scope.sub=false;
+				$scope.subcat=ret_data.result;
+			}
+			
+		})
+	}
+
+	$scope.submit_data=function(){
+		var name=$('#name').val();
+		var phone_num=$('#phone_num').val();
+		var email=$('#email').val();
+		var category=$('#category :selected').text();
+		var subcategory=$('#subcategory :selected').text();
+		var message=$('#message').val();
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		if(name==""){
+			alert("Please enter the name");
+		}
+		else if(phone_num==""){
+			alert("Please enter phone number");
+		}
+		else if(phone_num.length<10 || phone_num.length>10){
+			alert("Please enter valid phone number");
+		}
+		else if(!re.test(email)){
+			alert("Please enter valid email id");
+		}
+		else if(category==""){
+			alert("Please select category");
+		}
+		else if(subcategory==""){
+			alert("Please select subcategory");
+		}
+		else if(message==""){
+			alert("Please enter message");
+		}
+		else{
+			Email_send.email(name,phone_num,email,category,subcategory,message).then(function(data){
+				alert("Send Successfully");
+			});
+		}
+
+		
+
+		
+	}
+
+
+}])
+
+.controller('ContactCtrl', ['$scope','$stateParams','$state','$ionicLoading','Email_send', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams,$state,$ionicLoading,Email_send) {
+	$('#name').val("");
+	$('#phone_num').val("");
+	$('#email').val("");
+	$('#message').val("");
+	
+
+	$scope.Contact=function(){
+		var name=$('#name1').val();
+		var phone_num=$('#phone_num1').val();
+		var email=$('#email1').val();
+		var category="ContactUs";
+		var subcategory="ContactUs";
+		var message=$('#message1').val();
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		if(name==""){
+			alert("Please enter the name");
+		}
+		else if(phone_num==""){
+			alert("Please enter phone number");
+		}
+		else if(phone_num.length<10 || phone_num.length>10){
+			alert("Please enter valid phone number");
+		}
+		else if(!re.test(email)){
+			alert("Please enter valid email id");
+		}
+		else if(message==""){
+			alert("Please enter message");
+		}
+		else{
+			Email_send.email(name,phone_num,email,category,subcategory,message).then(function(data){
+				alert("Send Successfully");
+			});
+		}
+
+		
+
+		
+	}
+
+
+}])
+
+.controller('FeedbackCtrl', ['$scope','$stateParams','$state','$ionicLoading','Email_send',// The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+// You can include any angular dependencies as parameters for this function
+// TIP: Access Route Parameters for your page via $stateParams.parameterName
+function ($scope, $stateParams,$state,$ionicLoading,Email_send) {
+	$('#name').val("");
+	$('#phone_num').val("");
+	$('#email').val("");
+	$('#message').val("");
+	$scope.send=function(){
+		var name=$('#name2').val();
+		var phone_num=$('#phone_num2').val();
+		var email=$('#email2').val();
+		var category="ContactUs";
+		var subcategory="ContactUs";
+		var message=$('#message2').val();
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		if(name==""){
+			alert("Please enter  name");
+		}
+		else if(phone_num==""){
+			alert("Please enter phone number");
+		}
+		else if(phone_num.length<10 || phone_num.length>10){
+			alert("Please enter valid phone number");
+		}
+		else if(!re.test(email)){
+			alert("Please enter valid email id");
+		}
+		else if(message==""){
+			alert("Please enter message");
+		}
+		else{
+			Email_send.email(name,phone_num,email,category,subcategory,message).then(function(data){
+				alert("Send Successfully");
+			});
+		}
+
+		
+
+		
+	}
+
+
+}])
+
+.controller('mainCtrl', ['$scope','$stateParams','$state','$ionicLoading','Cat_list','getPost',
+	function($scope, $stateParams,$state,$ionicLoading,Cat_list,getPost){
+		$scope.Error=true;
+		$ionicLoading.show({
+			content: 'Loading',
+			animation: 'fade-in',
+			showBackdrop: true,
+			maxWidth: 200,
+			showDelay: 0
+		});
+		Cat_list.cat_list().then(function(data){
+			var ret=JSON.parse(data);
+			$scope.cat=ret.result;
+		})
+
+		getPost.get_post().then().then(function(data){
+			$ionicLoading.hide();
+
+			var ret=JSON.parse(data);
+			$scope.listdata=ret.result;
+		})
+		$scope.fullDesc=function(index,id){
+			$state.go('tabsController.detail',{val_index:id});
+		}
+
+
+		
+
+		$scope.show_category=function(category){
+			$ionicLoading.show({
+				content: 'Loading',
+				animation: 'fade-in',
+				showBackdrop: true,
+				maxWidth: 200,
+				showDelay: 0
+			});
+
+			getPost.get_post(category).then().then(function(data){
+				$ionicLoading.hide();
+				var ret1=JSON.parse(data);
+				if(ret1.flag=="unsuccess"){
+					$scope.ListVal=true;
+					$scope.Error=false;
+				}
+				else{
+					$scope.ListVal=false;
+					$scope.Error=true;
+					$scope.listdata=ret1.result;
+				}
+				
+			})
+			$scope.fullDesc=function(index,id){
+				$state.go('tabsController.detail',{val_index:id});
+			}
+
+			
+		}
+
+	}])
+
+
